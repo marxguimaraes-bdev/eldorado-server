38d3c0e153f722e4690155e3ee7d016cbe743ee9
Marx Guimaraes <fidel.guimaraes@linx3.com>
2021-04-10 19:18:51 -0400

    feat: add cross-origin header handler

--- server/server.js
+++ server/server.js
@@ -12,6 +12,14 @@ console.log('Initializing API');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use('/*', (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin' , '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+});
+
 app.use('/category', categoryRouter);
 app.use('/device', deviceRouter);
 
